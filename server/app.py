7e1c21db084659259d9b74b28c3308630d6c8833
Ryan Rooney <manythanks089@gmail.com>
2026-06-26 17:08:12 -0400

    final solution

--- server/app.py
+++ server/app.py
@@ -15,6 +15,58 @@ class ClearSession(Resource):
 
         return {}, 204
 
+class Signup(Resource):
+
+    def post(self):
+        request_json = request.get_json()
+
+        username = request_json.get('username')
+        password = request_json.get('password')
+
+        user = User(
+            username=username
+            )
+        user.password_hash = password
+
+        db.session.add(user)
+        db.session.commit()
+        session['user_id'] = user.id
+        
+        return UserSchema().dump(user), 201
+
+class Login(Resource):
+
+    def post(self):
+
+        user = User.query.filter(User.username == request.get_json()['username']).first()
+        if user:
+            session['user_id'] = user.id
+            return UserSchema().dump(user), 200
+        else:
+            return 401
+
+class CheckSession(Resource):
+
+    def get(self):
+
+        user = User.query.filter(User.id == session.get('user_id')).first() # User.id means the id column of the "User" table within the User class. session.get('user_id') retrieves the user's logged in session id number and assigns it to the User.id
+
+        if user:
+            return UserSchema().dump(user), 200
+        else:
+            return {}, 204
+
+class Logout(Resource):
+
+    def delete(self):
+        session['user_id'] = None
+        return {}
+
+
+api.add_resource(Signup, '/signup')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 
 if __name__ == '__main__':
